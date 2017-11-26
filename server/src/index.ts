efceff6f55c7e32cf4c59eb06c05b3aa0da55eda
hath995 <hathlesstroubles@gmail.com>
2017-11-26 10:50:58 -0800

    add cors to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import { apiRouter } from './api';
 import { appMiddleware, errorHandler } from './middleware';
 import logger from './util/logger';
 
 let app = express();
+app.use(cors());
 app.use(appMiddleware(app));
 app.use('/api', apiRouter);
 app.use(errorHandler);
